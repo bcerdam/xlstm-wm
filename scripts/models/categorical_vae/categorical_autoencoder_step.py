031e8c20a3d22381bdab5d5444eb1ca193db7f54
bcerdam <bcmardini@gmail.com>
2026-02-20 22:12:19 -0300

    loss fns

--- scripts/models/categorical_vae/categorical_autoencoder_step.py
+++ scripts/models/categorical_vae/categorical_autoencoder_step.py
@@ -13,7 +13,7 @@ def autoencoder_fwd_step(categorical_encoder:CategoricalEncoder,
                          sequence_length:int, 
                          latent_dim:int, 
                          codes_per_latent:int,
-                         lpips_loss_fn:lpips.LPIPS) -> tuple[torch.Tensor, torch.Tensor]:
+                         lpips_loss_fn:lpips.LPIPS) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     
     categorical_encoder.train()
     categorical_decoder.train()
@@ -37,4 +37,4 @@ def autoencoder_fwd_step(categorical_encoder:CategoricalEncoder,
         perceptual_loss = lpips_loss_fn(observations_batch.view(-1, 3, 64, 64), reconstructed_observations_batch.view(-1, 3, 64, 64)).mean()
         reconstruction_loss = reconstruction_loss + 0.2 * perceptual_loss
     
-    return reconstruction_loss, latents_sampled_batch
+    return reconstruction_loss, latents_sampled_batch, latents_batch
--- scripts/models/dynamics_modeling/dynamics_model_step.py
+++ scripts/models/dynamics_modeling/dynamics_model_step.py
@@ -14,24 +14,24 @@ def dm_fwd_step(dynamics_model:XLSTM_DM,
                 batch_size:int, 
                 sequence_length:int,
                 latent_dim:int, 
-                codes_per_latent:int, 
-                latents_sampled_batch:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+                codes_per_latent:int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
 
     dynamics_model.train()
     with torch.autocast(device_type='cuda', dtype=torch.bfloat16, enabled=True):
         next_latents_pred, rewards_pred, terminations_pred, hidden_state = dynamics_model.forward(tokens_batch=tokens_batch)
 
         next_latents_pred = next_latents_pred.view(size=(batch_size, sequence_length, latent_dim, codes_per_latent))
-        latents_batch = latents_batch.view(size=(batch_size, sequence_length, latent_dim, codes_per_latent))
 
-        rewards_loss = mse_loss(input=rewards_pred.squeeze(dim=-1), target=rewards_batch.float())
+        rewards_loss = mse_loss(input=rewards_pred[:, :-1].squeeze(dim=-1), target=rewards_batch[:, 1:].float())
+        terminations_loss = binary_cross_entropy_with_logits(input=terminations_pred[:, :-1].squeeze(dim=-1), target=terminations_batch[:, 1:].float())
 
-        terminations_loss = binary_cross_entropy_with_logits(input=terminations_pred.squeeze(dim=-1), target=terminations_batch.float())
+        post_logits = latents_batch[:, 1:]
+        prior_logits = next_latents_pred[:, :-1]
 
-        dynamics_kl = kl_divergence(OneHotCategorical(logits=latents_batch.detach()), OneHotCategorical(logits=next_latents_pred))
+        dynamics_kl = kl_divergence(OneHotCategorical(logits=post_logits.detach()), OneHotCategorical(logits=prior_logits))
         dynamics_loss = torch.clamp(dynamics_kl.sum(dim=-1).mean(), min=1.0)
 
-        representation_kl = kl_divergence(OneHotCategorical(logits=latents_batch), OneHotCategorical(logits=next_latents_pred.detach()))
+        representation_kl = kl_divergence(OneHotCategorical(logits=post_logits), OneHotCategorical(logits=prior_logits.detach()))
         representation_loss = torch.clamp(representation_kl.sum(dim=-1).mean(), min=1.0)
-        
+
     return rewards_loss, terminations_loss, dynamics_loss, representation_loss
