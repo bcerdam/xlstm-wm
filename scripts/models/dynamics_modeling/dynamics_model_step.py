ec25da20387bfef542d394873ce5cb58e6bd549d
bcerdam <bcmardini@gmail.com>
2026-02-20 21:46:22 -0300

    kl dist

--- scripts/models/dynamics_modeling/dynamics_model_step.py
+++ scripts/models/dynamics_modeling/dynamics_model_step.py
@@ -1,5 +1,7 @@
 import torch
-from torch.nn.functional import mse_loss, binary_cross_entropy_with_logits, kl_div
+from torch.nn.functional import mse_loss, binary_cross_entropy_with_logits
+from torch.distributions import OneHotCategorical
+from torch.distributions.kl import kl_divergence
 from typing import Tuple
 from .xlstm_dm import XLSTM_DM
 
@@ -26,12 +28,10 @@ def dm_fwd_step(dynamics_model:XLSTM_DM,
 
         terminations_loss = binary_cross_entropy_with_logits(input=terminations_pred.squeeze(dim=-1), target=terminations_batch.float())
 
-        latents_sampled_batch = latents_sampled_batch.view(size=(batch_size, sequence_length, latent_dim, codes_per_latent))
+        dynamics_kl = kl_divergence(OneHotCategorical(logits=latents_batch.detach()), OneHotCategorical(logits=next_latents_pred))
+        dynamics_loss = torch.clamp(dynamics_kl.sum(dim=-1).mean(), min=1.0)
 
-        dynamics_kl = kl_div(input=next_latents_pred, target=latents_sampled_batch.detach(), reduction='batchmean')
-        dynamics_loss = torch.clamp(dynamics_kl, min=1.0)
-
-        representation_kl = kl_div(input=next_latents_pred.detach(), target=latents_sampled_batch, reduction='batchmean')
-        representation_loss = torch.clamp(representation_kl, min=1.0)
+        representation_kl = kl_divergence(OneHotCategorical(logits=latents_batch), OneHotCategorical(logits=next_latents_pred.detach()))
+        representation_loss = torch.clamp(representation_kl.sum(dim=-1).mean(), min=1.0)
         
     return rewards_loss, terminations_loss, dynamics_loss, representation_loss
