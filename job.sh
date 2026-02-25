7b1010fe2c48840cb58de2a37e04bc7cc1f52977
bcerdam <bcmardini@gmail.com>
2026-02-25 19:17:27 -0300

    less cpus, fixed gpu print

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=output_%j.out
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 
 echo "--------------------------"
 echo "--- GPU Information ---"
@@ -16,16 +16,6 @@ echo "Memory Used (before script): $MEM_USED"
 echo "--------------------------"
 
 echo "Job started on $(date)"
-
-echo "--------------------------"
-echo "--- GPU Information ---"
-GPU_NAME=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader)
-MEM_TOTAL=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader)
-MEM_USED=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader)
-echo "GPU Name: $GPU_NAME"
-echo "Total Memory: $MEM_TOTAL"
-echo "Memory Used (before script): $MEM_USED"
-echo "--------------------------"
 
 RUN_DIR="output/run_$SLURM_JOB_ID"
 mkdir -p $RUN_DIR
