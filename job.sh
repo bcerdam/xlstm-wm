46176b60d47b5319e110f6054efb0bb89c903e13
bcerdam <bcmardini@gmail.com>
2026-02-25 11:39:23 -0300

    job

--- job.sh
+++ job.sh
@@ -7,4 +7,4 @@
 RUN_DIR="output/run_$SLURM_JOB_ID"
 mkdir -p $RUN_DIR
 
-python train.py --run_dir $RUN_DIR --train_wm.wm_batch_size 32 --train_wm.world_model_learning_rate 0.0005
+python train.py --run_dir $RUN_DIR $@
